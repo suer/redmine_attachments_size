5cf9f3c849f0653e71f25347f5d5cac82188d9f6
suer <sue@nagoya-u.jp>
2011-03-20 00:07:30 +0900

    move to admin_menu

--- init.rb
+++ init.rb
@@ -8,5 +8,5 @@ Redmine::Plugin.register :redmine_attachments_size do
   url 'http://example.com/path/to/plugin'
   author_url 'http://d.hatena.ne.jp/suer'
 
-  menu :top_menu, :attachments_size, {:controller => 'attachments_size'}, :public => false, :caption => 'Attachments size'
+  menu :admin_menu, :attachments_size, {:controller => 'attachments_size'}, :caption => 'Attachments size'
 end
