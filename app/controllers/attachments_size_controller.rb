86b92443a5e428ebedfab5272fc4ddd13a31fc9d
suer <sue@nagoya-u.jp>
2011-04-06 00:54:50 +0900

    container id

--- app/controllers/attachments_size_controller.rb
+++ app/controllers/attachments_size_controller.rb
@@ -12,18 +12,24 @@ class AttachmentsSizeController < ApplicationController
       when 'Project'
         @size_by_projects[attachment.container_id][attachment.container_type] ||= 0
         @size_by_projects[attachment.container_id][attachment.container_type] += attachment.filesize
+        @size_by_projects[attachment.container_id][:total] ||= 0
+        @size_by_projects[attachment.container_id][:total] += attachment.filesize
       when 'WikiPage'
-        @size_by_projects[attachment.container_id][attachment.container_type] ||= 0
+        @size_by_projects[WikiPage.find(attachment.container_id).wiki.project_id][attachment.container_type] ||= 0
         @size_by_projects[WikiPage.find(attachment.container_id).wiki.project_id][attachment.container_type] += attachment.filesize
+        @size_by_projects[WikiPage.find(attachment.container_id).wiki.project_id][:total] ||= 0
+        @size_by_projects[WikiPage.find(attachment.container_id).wiki.project_id][:total] += attachment.filesize
       when 'Document'
-        @size_by_projects[attachment.container_id][attachment.container_type] ||= 0
+        @size_by_projects[Document.find(attachment.container_id).project_id][attachment.container_type] ||= 0
         @size_by_projects[Document.find(attachment.container_id).project_id][attachment.container_type] += attachment.filesize
+        @size_by_projects[Document.find(attachment.container_id).project_id][:total] ||= 0
+        @size_by_projects[Document.find(attachment.container_id).project_id][:total] += attachment.filesize
       when 'Issue'
-        @size_by_projects[attachment.container_id][attachment.container_type] ||= 0
+        @size_by_projects[Issue.find(attachment.container_id).project_id][attachment.container_type] ||= 0
         @size_by_projects[Issue.find(attachment.container_id).project_id][attachment.container_type] += attachment.filesize
+        @size_by_projects[Issue.find(attachment.container_id).project_id][:total] ||= 0
+        @size_by_projects[Issue.find(attachment.container_id).project_id][:total] += attachment.filesize
       end
-      @size_by_projects[attachment.container_id][:total] ||= 0
-      @size_by_projects[attachment.container_id][:total] += attachment.filesize
       @attachment_container_types << attachment.container_type unless @attachment_container_types.include?(attachment.container_type)
     end
   end
