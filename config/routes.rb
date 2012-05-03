840943f596f66d11c318a6c9ff83e83c70247e17
suer <suetsugu.r@gmail.com>
2012-05-04 03:04:11 +0900

    for redmine2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,1 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'attachments_size/:action', :controller => 'attachments_size'
-end
+match 'attachments_size/:action', :controller => 'attachments_size'
